767f0e920b090e7b3118a1583e23a3506661bdf7
haloislet <haloislet@gmail.com>
2020-09-12 02:02:54 +0800

    :sparkles: add getMines getExts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const mineExtMap = Object.keys(db)
   );
 
 const extMineMap = Object.keys(mineExtMap).reduce(
-  (acc: { [key: string]: string[] }, mine: string) => {
+  (acc: { [key: string]: string }, mine: string) => {
     const extensions: string[] = mineExtMap[mine];
     const extensionItemMap = Object.fromEntries(
       extensions.map((extension: string) => [extension, mine])
@@ -30,14 +30,24 @@ const extMineMap = Object.keys(mineExtMap).reduce(
   {}
 );
 class Mine {
-  static getMine(ext: string): string[] {
+  static getMine(ext: string): string {
     return extMineMap[ext];
+  }
+  static getMines(...exts: Array<string | string[]>): string[] {
+    const allExts = exts.flat();
+    return [...new Set(allExts.map(ext => extMineMap[ext]).flat())];
   }
   static getExt(mine: string): string[] {
     return mineExtMap[mine];
+  }
+  static getExts(...mines: Array<string | string[]>): string[] {
+    const allMines = mines.flat();
+    return [...new Set(allMines.map(mine => mineExtMap[mine]).flat())];
   }
 }
 
 export default Mine;
 export const getMine = Mine.getMine;
 export const getExt = Mine.getExt;
+export const getMines = Mine.getMines;
+export const getExts = Mine.getExts;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { getMine, getExt } from '../src';
+import { getMine, getExt, getExts, getMines } from '../src';
 
 test('getMine', () => {
   expect(getMine('js')).toBe('application/javascript');
@@ -6,4 +6,32 @@ test('getMine', () => {
 
 test('getExt', () => {
   expect(getExt('application/javascript')).toEqual(['js', 'mjs']);
+});
+
+test('getMines', () => {
+  expect(getMines('js', 'html')).toEqual([
+    'application/javascript',
+    'text/html',
+  ]);
+  expect(getMines('js', ['html'])).toEqual([
+    'application/javascript',
+    'text/html',
+  ]);
+});
+
+test('getExts', () => {
+  expect(getExts('application/javascript', ['text/html'])).toEqual([
+    'js',
+    'mjs',
+    'html',
+    'htm',
+    'shtml',
+  ]);
+  expect(getExts('application/javascript', 'text/html')).toEqual([
+    'js',
+    'mjs',
+    'html',
+    'htm',
+    'shtml',
+  ]);
 });
